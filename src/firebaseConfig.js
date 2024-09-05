e2c18be758795df872042f98e83b3de596194946
Арутюнов Владимир <vladimir.arutyunov@tele2.ru>
2024-09-06 03:08:20 +0400

    Add Splash Screen

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCj8SmLpnOR_6HXOHCJptg1oMHIeb4aSec",
-  authDomain: "whale-finance-10349.firebaseapp.com",
-  projectId: "whale-finance-10349",
-  storageBucket: "whale-finance-10349.appspot.com",
-  messagingSenderId: "37818570043",
-  appId: "1:37818570043:web:c274b8e42f98dbe993931e",
-  measurementId: "G-VH22X2R7GM",
+  apiKey: "AIzaSyC0P4jUnO4tmdmsryb5jkJDEUQOHoELDjo",
+  authDomain: "while-finance-r.firebaseapp.com",
+  projectId: "while-finance-r",
+  storageBucket: "while-finance-r.appspot.com",
+  messagingSenderId: "1040548266280",
+  appId: "1:1040548266280:web:55b0bcbf097818f688101d",
+  measurementId: "G-12CL13W9JF",
 };
 
 const app = initializeApp(firebaseConfig);
